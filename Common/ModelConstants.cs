331f7e7bfd0f662b365f589ebf7d2426a66cdd4a
Lyuboslav Vezenkov <luboslav.vezenkov6907@gmail.com>
2024-10-27 13:28:50 +0200

    Initial commit

---
+++ Common/ModelConstants.cs
@@ -0,0 +1,49 @@
+﻿namespace KickShop.Common
+{
+    public static class ModelConstants
+    {
+        public static class Brand
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 100;
+
+            public const int CountryMinLength = 3;
+            public const int CountryMaxLength = 100;
+        }
+        public static class CartItem
+        {
+            public const int QuantityRangeMin = 1;
+            public const int QuantityRangeMax = 999;
+        }
+        public static class Category
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 50;
+        }
+        public static class Order
+        {
+            public const int TotalAmountMin = 0;
+            public const int TotalAmountMax = 99999;
+
+            public const string DateTimeFormat = "yyyy/MM/dd HH:mm";
+        }
+        public static class Product
+        {
+            public const int NameMinLength = 5;
+            public const int NameMaxLength = 100;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 1000;
+
+            public const int PriceRangeMin = 0;
+            public const int PriceRangeMax = 9999;
+
+            public const int QuantityRangeMin = 0;
+            public const int QuantityRangeMax = 1000;
+        }
+        public static class ShoppingCart
+        {
+            public const string DateTimeFormat = "yyyy/MM/dd HH:mm";
+        }
+    }
+}
---
+++ Models/Brand.cs
@@ -0,0 +1,27 @@
+﻿using KickShop.Common;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace KickShop.Models
+{
+    public class Brand
+    {
+        public Brand()
+        {
+            BrandId = Guid.NewGuid();
+        }
+        [Comment("The Unique Identifier")]
+        [Key]
+        public Guid BrandId { get; set; }
+        [Comment("The Name Of The Brand")]
+        [Required]
+        [MinLength(ModelConstants.Brand.NameMinLength)]
+        [MaxLength(ModelConstants.Brand.NameMaxLength)]
+        public string Name { get; set; }
+        [Comment("The Country Of Origin Of The The Brand")]
+        [Required]
+        [MinLength(ModelConstants.Brand.CountryMinLength)]
+        [MaxLength(ModelConstants.Brand.CountryMaxLength)]
+        public string Country { get; set; }
+    }
+}
